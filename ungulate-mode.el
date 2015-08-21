6b03f72085d49c0ba02f8d177f925e69e24b163d
Ian Clark Smith <ian@sumisu.co>
2015-08-20 21:14:39 -0400

    Package information

--- ungulate-mode.el
+++ ungulate-mode.el
@@ -1,3 +1,29 @@
+;;; ungulate-mode.el --- Minor mode for RhinoPython scripting
+
+;; Copyright 2015 Ian Clark Smith
+
+;; Author: Ian Clark Smith <ian@sumisu.co>
+;; URL: http://github.com/ianclarksmith/ungulate-mode
+;; Keywords: tools
+;; Package-Requires: ((request "0.2.0"))
+
+;;; Commentary:
+
+;; A minor mode for editing RhinoPython scripts
+;;
+;; Usage:
+;; - To use, enter the command StartAtomEditorListener from within Rhinoceros
+;; - Activate ungulate-mode in a buffer containing RhinoPython code
+;; - Run `ungulate-eval-buffer' or C-c C-c in an ungulate-mode buffer
+;;
+;; Troubleshooting:
+;; - Note that on some systems the loopback address (localhost) defaults to IPv6
+;;   although the Rhino editor server only listens on IPv4. To ensure that
+;;   requests are dispatched properly, create or edit ~/.curlrc and add the line
+;;   "ipv4" without quotes.
+
+;;; Code:
+
 (require 'request)
 (require 'json)
 
@@ -20,7 +46,9 @@ Set whenever `ungulate-rhino-is-listening' is run.")
 
 (defun ungulate-eval-buffer ()
   "Evaluate the contents of the current buffer in Rhino.
-To do this, the buffer is saved as a temporary file with a .py extension and sent to Rhino to run as a Python script."
+
+To do this, the buffer is saved as a temporary file with a .py extension and
+sent to Rhino to run as a Python script."
   (interactive)
   (let* ((temp-file (make-temp-file "rhinoscript" nil ".py")))
     (write-region nil nil temp-file)
@@ -55,3 +83,4 @@ To do this, the buffer is saved as a temporary file with a .py extension and sen
   (start-process "" nil "open" ungulate-rhino-path))
 
 (provide 'ungulate-mode)
+;;; ungulate-mode.el ends here
